b66b9487202369dcdbda12cbf58012316c68f8fd
OtanaJWilson <78755889+fixeg683@users.noreply.github.com>
2026-04-08 12:49:08 +0300

    Refactor download link for file availability

--- src/pages/Downloads.jsx
+++ src/pages/Downloads.jsx
@@ -34,11 +34,7 @@ export default function Downloads() {
             <strong>{file.name}</strong>
             <br />
             {file.file ? (
-              
-                href={file.file}
-                download
-                style={{ color: "green", fontWeight: "bold" }}
-              >
+              <a href={file.file} download style={{ color: "green", fontWeight: "bold" }}>
                 Download
               </a>
             ) : (
