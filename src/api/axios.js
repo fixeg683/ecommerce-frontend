b1c147457930d830f11593af845af42a20172391
fixeg683 <jacobotana96@gmail.com>
2026-05-22 23:34:51 +0300

    modified src/api/axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const API = axios.create({
   baseURL:
     import.meta.env.VITE_API_URL ||
-    "http://127.0.0.1:8000/api",
+    "https://backend-ecommerce-3-href.onrender.com/api",
 });
 
 API.interceptors.request.use((config) => {
@@ -15,5 +15,14 @@ API.interceptors.request.use((config) => {
 
   return config;
 });
+
+API.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    console.error("API Error:", error);
+
+    return Promise.reject(error);
+  }
+);
 
 export default API;
