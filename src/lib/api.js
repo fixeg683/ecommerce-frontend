6d98f85608f290c364d36af8e9f4d342b096c029
fixeg683 <jacobotana96@gmail.com>
2026-05-27 08:20:17 +0300

    modified src/lib/api.js

--- src/lib/api.js
+++ src/lib/api.js
@@ -8,11 +8,18 @@ const api = axios.create({
 
 // Attach bearer token from localStorage
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
+  const token = localStorage.getItem('token') || localStorage.getItem('access');
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
   return config;
 });
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default api;
